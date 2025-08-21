91c4fbb8baeb45082cfe1b4477401f4de2d928ae
Lakr <git@qaq.wiki>
2025-08-21 23:51:28 +0800

    Update Mode Loading Animation

--- Example/WhisperKitExample/ExampleApp.swift
+++ Example/WhisperKitExample/ExampleApp.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ExampleApp: App {
+    @State var initialized = false
+
     var body: some Scene {
         WindowGroup {
-            MainView()
+            if initialized {
+                MainView()
+            } else {
+                ProgressView()
+                    .onAppear {
+                        DispatchQueue.global().async {
+                            do {
+                                try WhisperKitManager.shared.initialize()
+                                DispatchQueue.main.async {
+                                    initialized = true
+                                }
+                            } catch {
+                                fatalError(error.localizedDescription)
+                            }
+                        }
+                    }
+                    .frame(width: 800, height: 600)
+            }
         }
     }
 }
---
+++ Example/WhisperKitExample/WhisperKitManager.swift
@@ -0,0 +1,40 @@
+//
+//  WhisperKitManager.swift
+//  WhisperKitExample
+//
+//  Created by 秋星桥 on 8/21/25.
+//
+
+import WhisperKit
+import Foundation
+
+class WhisperKitManager {
+    static let shared = WhisperKitManager()
+    private(set) var whisperKit: WhisperKit?
+    private(set) var vadService: VADService?
+
+    private init() {}
+
+    func initialize() throws {
+        vadService = try VADService()
+        print("VADService initialized at \(vadService!)")
+
+        guard let modelURL = Bundle.main
+            .url(
+                forResource: "ggml-large-v3-turbo-q8_0",
+                withExtension: "bin",
+                subdirectory: "Models"
+            ),
+            FileManager.default.fileExists(atPath: modelURL.path)
+        else {
+            throw NSError(
+                domain: "WhisperKit",
+                code: -1,
+                userInfo: [NSLocalizedDescriptionKey: "Model file not found"]
+            )
+        }
+
+        whisperKit = try WhisperKit(modelURL: modelURL)
+        print("WhisperKit initialized successfully")
+    }
+}
